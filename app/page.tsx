5ec8c38d5c972b68a8a3fb444516c4cbee1433e1
KBG1 <bumdoly200@naver.com>
2025-09-11 14:48:51 +0900

    도넛게임(아직미완성)

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,18 @@ import React from "react";
 function Home() {
   return (
     <>
-      <div className="w-full text-center p-12">
+      <div className="w-full text-center p-12 flex flex-col items-center">
         <Link
           href="/game"
           className="text-2xl font-bold bg-[#6ead79] p-4 text-white rounded-md hover:bg-[#4d8750] cursor-pointer"
         >
-          게임하러 가기 버튼
+          선 연결 게임하러 가기 버튼
+        </Link>
+        <Link
+          href="/game2"
+          className="mt-12 text-2xl font-bold bg-[#6ead79] p-4 text-white rounded-md hover:bg-[#4d8750] cursor-pointer"
+        >
+          도넛 게임하러 가기 버튼
         </Link>
       </div>
     </>
